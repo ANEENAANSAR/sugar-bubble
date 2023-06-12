b66642a94e5fe19597d8620fc1deb8424214eae3
aneena ansar <aneenaanzz123@gmail.com>
2023-06-12 20:18:53 +0530

    index.html

---
+++ script.js
@@ -0,0 +1,15 @@
+const nav = document.getElementById('icon5');
+const bar = document.getElementById('menu');
+
+
+bar.addEventListener('click',()=>{
+    nav.classList.toggle('jav')
+})
+
+const icon = document.getElementById('list');
+const list = document.getElementById('span1');
+
+
+bar.addEventListener('click',()=>{
+    nav.classList.toggle('muff')
+})
